3c32c206e3bc1b9c4ffb38aa2005b2a65b119dc9
becmorim <kyuuyoutubr@gmail.com>
2025-04-24 09:27:25 -0300

    termino

--- main.js
+++ main.js
@@ -8,70 +8,74 @@ class Personagem {
         this.#força = força;
         this.#mana = mana;
     }
-
     get vida() {
         return this.#vida;
     }
-
     set vida(novaVida) {
         this.#vida = novaVida;
     }
-
     get força() {
         return this.#força;
     }
-
     set força(novaForça) {
         this.#força = novaForça;
     }
-
     get mana() {
         return this.#mana;
     }
-
     set mana(novaMana) {
         this.#mana = novaMana;
     }
 }
 
 class Guerreiro extends Personagem {
-    atacar() {
-        console.log("guerreiro atacou");
+    atacar(oponente) {
+        const dano = this.vida * 0.10;
+        oponente.vida -= dano;
+        console.log(`Guerreiro atacou e causou ${dano} de dano. Vida do oponente: ${oponente.vida.toFixed(2)}`);
     }
-
     defender() {
-        console.log("guerreiro defendeu");
+        console.log("Guerreiro defendeu");
     }
-
     usarEscudo() {
         console.log("Guerreiro usou escudo");
+    }
+    agir(oponente) {
+        const acao = Math.floor(Math.random() * 3); 
+        switch (acao) {
+            case 0:
+                this.atacar(oponente);
+                break;
+            case 1:
+                this.defender();
+                break;
+            case 2:
+                this.usarEscudo();
+                break;
+        }
     }
 }
 
 class Mago extends Personagem {
-    atacar() {
-        console.log("mago atacou");
+    atacar(oponente) {
+        const dano = this.vida * 0.10;
+        oponente.vida -= dano;
+        console.log(`Mago atacou e causou ${dano} de dano. Vida do oponente: ${oponente.vida.toFixed(2)}`);
     }
-
     defender() {
-        console.log("mago se defendeu");
+        console.log("Mago se defendeu");
     }
-
     usarMagia() {
-        console.log("mago usou magia");
+        console.log("Mago usou magia");
     }
-}
-
-class Arqueiro extends Personagem {
-    atacar() {
-        console.log("arqueiro atacou");
-    }
-
-    defender() {
-        console.log("arqueiro defendeu");
-    }
-
-    recarregar() {
-        console.log("arqueiro recarregou");
+    agir(oponente) {
+        const acao = Math.floor(Math.random() * 3); 
+        switch (acao) {
+            case 0:
+                this.atacar(oponente);
+                break;
+            case 1:
+                this.defender();
+        }
     }
 }
